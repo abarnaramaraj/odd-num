7569c2c2dea88519f175a42dfe3efadb40fdfeda
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-06 12:00:34 +0530

    Create Oddnum.java

---
+++ Oddnum.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+public class Oddnum
+{
+public static void main(String[] args)
+{
+int a,b,x,i,count=0;
+Scanner sc=new Scanner(System.in);
+System.out.println();
+a=sc.nextInt();
+b=sc.nextInt();
+for(i=0;i<=b;i++)
+{
+x=sc.nextInt();
+if(x%2!=0)
+{
+count=count+1;
+if(count=b)
+{
+System.out.println(x)
+}
+}
+}
+}
+}
