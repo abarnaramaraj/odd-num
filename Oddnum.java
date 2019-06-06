605d003571ed42b487e122b636caf49953aaec07
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-06 12:17:01 +0530

    Update Oddnum.java

--- Oddnum.java
+++ Oddnum.java
@@ -4,17 +4,17 @@ public class Oddnum
 {
 public static void main(String[] args)
 {
-int a,b,x,i,count=0;
+int p,s,x,i,count=0;
 Scanner sc=new Scanner(System.in);
-a=sc.nextInt();
-b=sc.nextInt();
-for(i=0;i<a;i++)
+p=sc.nextInt();
+s=sc.nextInt();
+for(i=0;i<p;i++)
 {
 x=sc.nextInt();
 if(x%2!=0)
 {
 count=count+1;
-if(count==b)
+if(count==s)
 {
 System.out.println(x);
 }
