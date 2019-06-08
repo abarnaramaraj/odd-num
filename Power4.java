66fbd1038281775263937e76f510c17b7059bd97
abarnaramaraj <47477481+abarnaramaraj@users.noreply.github.com>
2019-06-08 09:49:17 +0530

    Create Power4.java

---
+++ Power4.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+ public class Power4 {
+  public static void main(String[] args) {
+    double n,m;
+  Scanner sc=new Scanner(System.in);
+   n=sc.nextInt();
+  m=sc.nextInt();
+    
+   int result = (int)Math.pow(n, m);
+   System.out.println(result);
+
+  }
+}
